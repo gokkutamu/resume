6327b2c37f68c2e0268f2586c29026525a67cbd3
Tran Ngoc Tam <tran.ngoc.tam@uruca.jp>
2023-04-10 11:23:36 +0700

    update landing page

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -9,7 +9,7 @@ import { Navigate } from '../../config/data/navigate';
 export default function Navbar({ darkMode, click }) {
     const location = ReactDOM.useLocation(); //this.props.location || { pathname: '/' }
     const handleDarkMode = darkMode ? 'dark' : 'light';
-    const [active, setActive] = React.useState(location.pathname === '/' ? 'home' : location.pathname.slice(1, location.pathname.length));
+    const [active, setActive] = React.useState(location.pathname === '#' ? 'home' : location.pathname.slice(1, location.pathname.length));
 
     return (
         <nav className={`navbar navbar-expand-sm navbar-${handleDarkMode} headroom headroom--not-bottom headroom--pinned headroom--top`}>
@@ -22,15 +22,15 @@ export default function Navbar({ darkMode, click }) {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <Div.Box className="collapse navbar-collapse" id="navbarNav">
-                    <Div.Box component="ul" className="navbar-nav mx-auto">
+                    <ul className="navbar-nav mx-auto">
                         {Navigate.map((link, index) => (
                             <li className="nav-item" key={index}>
-                                <ReactDOM.Link to={link.to} onClick={() => setActive(link.active)} className={`nav-link ${link.active === active && !link.type && 'active'}`}>
+                                <a href={link.to} onClick={() => setActive(link.active)} className={`nav-link ${link.active === active && !link.type && 'active'}`}>
                                     <span data-target={link.active}>{link.name}</span>
-                                </ReactDOM.Link>
+                                </a>
                             </li>
                         ))}
-                    </Div.Box>
+                    </ul>
                     <ul className="navbar-nav">
                         <Div.Box className="ml-lg-4">
                             <ToggleDarkMode darkMode={darkMode} handleClick={click} />
